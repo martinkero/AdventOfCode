40772ecacdac812065cd572848ac5acb12e9bcec
martinkero <martin.kero@clearit.se>
2017-12-07 19:31:13 +0100

    day6

---
+++ day6/day6.java
@@ -0,0 +1,62 @@
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class day6 {
+
+    public static void main(String[] args) {
+        List<String> input = new ArrayList<>();
+        try {
+            input = utils.readInputAsList("day6/day6.in");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        List<Integer> config = utils.stringListToIntegerList(input);
+
+        List<String> seenConfigs = new ArrayList<>();
+        seenConfigs.add(config.toString());
+
+        int cycles = 0;
+        while (true) {
+            reallocate(config);
+            cycles++;
+            if (configIsSeen(seenConfigs, config.toString())) {
+                break;
+            }
+            seenConfigs.add(config.toString());
+        }
+
+        System.out.println(cycles);
+    }
+    private static boolean configIsSeen(List<String> seenConfigs, String config) {
+        for (String seenConfig : seenConfigs) {
+            if (seenConfig.equals(config)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void reallocate(List<Integer> config) {
+        int highestValue = 0;
+        int highestValueIndex = 0;
+        for (int i = 0; i<config.size(); i++) {
+            int value = config.get(i);
+            if (value > highestValue) {
+                highestValue = value;
+                highestValueIndex = i;
+            }
+        }
+
+        config.set(highestValueIndex, 0);
+        int startingPoint = highestValueIndex+1;
+        while (highestValue>0) {
+            for (int i = startingPoint; i<config.size() && highestValue>0; i++) {
+                int currentValue = config.get(i);
+                config.set(i, currentValue+1);
+                highestValue--;
+            }
+            startingPoint = 0;
+        }
+    }
+}
