ce5b6b2e46071c5ba493c4a7d1bf1a06d77c40c7
martinkero <martin.kero@clearit.se>
2017-12-11 15:13:25 +0100

    day9

---
+++ day9/Day9.java
@@ -0,0 +1,82 @@
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        testProcessGroupScore();
+
+        String input = null;
+        try {
+            input = utils.readInputAsList("day9/Day9.in").toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println(processGroupScore(input));
+    }
+
+    private static int processGroupScore(String stream) {
+        int groupScore = 0;
+        int depth = 0;
+        boolean inGarbage = false;
+        boolean skipNext = false;
+
+        for (int i = 0; i < stream.length(); i++) {
+            String currentChar = String.valueOf(stream.charAt(i));
+
+            if (skipNext) {
+                skipNext = false;
+                continue;
+            }
+            if (inGarbage && (!currentChar.equals(">") && !currentChar.equals("!"))) {
+                continue;
+            }
+
+            switch (currentChar) {
+                case "{":
+                    depth++;
+                    break;
+                case "}":
+                    groupScore += depth;
+                    depth--;
+                    break;
+                case "<":
+                    inGarbage = true;
+                    break;
+                case ">":
+                    inGarbage = false;
+                    break;
+                case "!":
+                    if (inGarbage) {
+                        skipNext = true;
+                    }
+                    break;
+            }
+        }
+        return groupScore;
+    }
+
+    private static void testProcessGroupScore() {
+        Map<String, Integer> inputAndExpectedResults = new HashMap<>();
+        inputAndExpectedResults.put("{}", 1);
+        inputAndExpectedResults.put("{{{}}}", 6);
+        inputAndExpectedResults.put("{{},{}}", 5);
+        inputAndExpectedResults.put("{{{},{},{{}}}}", 16);
+        inputAndExpectedResults.put("{<a>,<a>,<a>,<a>}", 1);
+        inputAndExpectedResults.put("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9);
+        inputAndExpectedResults.put("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9);
+        inputAndExpectedResults.put("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3);
+
+        for (Map.Entry<String, Integer> inputAndExpectedResult : inputAndExpectedResults.entrySet()) {
+            String input = inputAndExpectedResult.getKey();
+            int expectedResult = inputAndExpectedResult.getValue();
+            int result = processGroupScore(input);
+            if (result != expectedResult) {
+                System.out.println("Test failed, input string: " + input + "\ngot " + result + ", expected " + expectedResult);
+                System.exit(1);
+            }
+        }
+    }
+}
