1ced4f00c2dedf7abced2a8347b4bbd44a109108
martinkero <martin.kero@clearit.se>
2017-12-19 15:57:46 +0100

    Day17 part2 dumb/quick version

--- day17/Day17.kt
+++ day17/Day17.kt
@@ -5,10 +5,8 @@ fun main(args: Array<String>) {
 
     println("Part 1:")
     println(identifySpinlockValueP1(2017, 345))
-    val time = System.currentTimeMillis()
     println("Part 2:")
     println(identifySpinlockValueP2(50000000, 345))
-    println(System.currentTimeMillis()-time)
 }
 
 data class CircularBuffer(val positions: MutableList<Int>, var currentPosition: Int)
@@ -20,9 +18,18 @@ fun identifySpinlockValueP1(totalInsertions: Int, stepsToTake: Int): Int {
 }
 
 fun identifySpinlockValueP2(totalInsertions: Int, stepsToTake: Int): Int {
-    val cb = CircularBuffer(mutableListOf(0), 0)
-    insertValues(cb, totalInsertions, stepsToTake)
-    return cb.positions[cb.positions.indexOf(0)+1]
+    var currentPosition = 0
+    var size = 1
+    var value = 0
+    for (i in (0..totalInsertions)) {
+        currentPosition = (currentPosition + stepsToTake) % size
+        currentPosition++
+        size++
+        if (currentPosition==1) {
+            value = i+1
+        }
+    }
+    return value
 }
 
 fun insertValues(cb: CircularBuffer, totalInsertions: Int, stepsToTake: Int) {
